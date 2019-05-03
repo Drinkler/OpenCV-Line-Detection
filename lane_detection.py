089add9fb1614611efe096e12534f27c26b5d3e3
Florian Drinkler <flo.drinkler@gmail.com>
2019-05-03 23:45:55 +0200

    better line display

--- lane_detection.py
+++ lane_detection.py
@@ -2,36 +2,68 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture('countryroad.mp4')
+def make_coordinates (image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
+def average_slope_intercept (image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
+def canny (image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
+    return canny
 
+def display_lines (image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
-    height = canny.shape[0]
+def region_of_interest (image):
+    height = image.shape[0]
     polygons = np.array([
         [(20, height), (1900, height), (1200, 600), (1120, 600)]
     ])
-    mask = np.zeros_like(canny)
+    mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    cropped_image = cv2.bitwise_and(canny, mask)
-    
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+cap = cv2.VideoCapture('countryroad.mp4')
 
-    # display lines
-    line_image = np.zeros_like(frame)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 10)
+while (cap.isOpened()):
+    _, frame = cap.read()
 
+    canny_image = canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=100)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', combo_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
